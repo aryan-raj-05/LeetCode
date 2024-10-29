6b1f1cb060b399081f9929e6550e313390c32dea
Aryan <aryan56raj342@gmail.com>
2024-10-29 14:24:20 +0530

    solved leetcode problem 13 roman to integer
    convert a given roman numeral to an int
    using a hashmap

---
+++ src/Problem0013_RomantoInteger/Solution.java
@@ -0,0 +1,31 @@
+package Problem0013_RomantoInteger;
+
+/*
+    LeetCode Problem 13: Roman to Integer
+    Given a Roman Numeral convert it into a valid integer
+ */
+
+import java.util.HashMap;
+
+public class Solution {
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> mp = new HashMap<>();
+        mp.put('I', 1);
+        mp.put('V', 5);
+        mp.put('X', 10);
+        mp.put('L', 50);
+        mp.put('C', 100);
+        mp.put('D', 500);
+        mp.put('M', 1000);
+
+        int ans = 0;
+        int i = 0;
+        while (i < s.length() - 1) {
+            if (mp.get(s.charAt(i)) < mp.get(s.charAt(i + 1)))  ans -= mp.get(s.charAt(i));
+            else                                                ans += mp.get(s.charAt(i));
+            i++;
+        }
+        ans += mp.get(s.charAt(i));
+        return ans;
+    }
+}
