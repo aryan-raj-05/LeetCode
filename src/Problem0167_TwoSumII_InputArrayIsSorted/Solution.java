a9483b44dd358dcbc1bda4f6dfada0051ea583dd
Aryan <aryan56raj342@gmail.com>
2024-10-30 19:46:34 +0530

    solved problem 0167 two sum II, input array is sorted

---
+++ src/Problem0167_TwoSumII_InputArrayIsSorted/Solution.java
@@ -0,0 +1,20 @@
+package Problem0167_TwoSumII_InputArrayIsSorted;
+
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int i = 0, j = numbers.length - 1;
+        int[] result = new int[2];
+        while (i < j) {
+            if (numbers[i] + numbers[j] == target) {
+                result[0] = i + 1;
+                result[1] = j + 1;
+                break;
+            } else if(numbers[i] + numbers[j] > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return result;
+    }
+}
