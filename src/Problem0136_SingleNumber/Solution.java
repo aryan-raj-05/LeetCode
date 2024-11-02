7c84597b467d0f04ed47fbb4695d7c47ec0702e8
Aryan <aryan56raj342@gmail.com>
2024-11-02 18:46:47 +0530

    solved problem 136, single number

---
+++ src/Problem0136_SingleNumber/Solution.java
@@ -0,0 +1,10 @@
+package Problem0136_SingleNumber;
+
+public class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for (int x: nums)
+            ans = ans ^ x;
+        return ans;
+    }
+}
