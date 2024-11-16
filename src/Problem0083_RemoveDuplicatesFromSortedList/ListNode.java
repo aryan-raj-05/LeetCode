c371452535cf4fb1ff95e17fb2d1ecafb8e1b08b
Aryan <aryan56raj342@gmail.com>
2024-11-17 00:42:51 +0530

    solved problem 83, remove duplicates from LL

---
+++ src/Problem0083_RemoveDuplicatesFromSortedList/ListNode.java
@@ -0,0 +1,17 @@
+package Problem0083_RemoveDuplicatesFromSortedList;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {}
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
