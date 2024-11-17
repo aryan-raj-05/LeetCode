dfa8a13d0daf06a999295f3479027e06811a67b7
Aryan <aryan56raj342@gmail.com>
2024-11-17 16:35:49 +0530

    solved problem 121, best time to buy and sell stocks

---
+++ src/Problem0121_BestTimeToBuyAndSellStock/Solution.java
@@ -0,0 +1,15 @@
+package Problem0121_BestTimeToBuyAndSellStock;
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        int minValue = Integer.MAX_VALUE;
+        int profit = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            minValue = Integer.min(prices[i], minValue);
+            profit = Integer.max(profit, prices[i] - minValue);
+        }
+
+        return profit;
+    }
+}
