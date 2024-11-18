9887aed899c2d26a7e097904dee1efc00ce8fc29
Aryan <aryan56raj342@gmail.com>
2024-11-18 10:26:12 +0530

    solved problem 219, contains duplicate 2

---
+++ src/Problem0219_ContainsDuplicateII/Solution.java
@@ -0,0 +1,17 @@
+package Problem0219_ContainsDuplicateII;
+
+import java.util.HashMap;
+
+public class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (hm.containsKey(nums[i]) && Math.abs(hm.get(nums[i]) - i) <= k)
+                return true;
+            hm.put(nums[i], i);
+        }
+
+        return false;
+    }
+}
