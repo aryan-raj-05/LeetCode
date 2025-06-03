9cfa96b8a92a90e6994d3b51ad3345e81d09c4f5
Aryan <161190212+aryan-raj-05@users.noreply.github.com>
2025-06-03 04:00:07 +0000

    include definition of Linked List

--- easy/0021_mergeTwoLists.cpp
+++ easy/0021_mergeTwoLists.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
-#include "ListNode.cpp"
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
 class Solution {
 public:
